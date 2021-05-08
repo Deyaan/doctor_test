642e4d8de5aa97906c4ca490accd7ae5f03deb33
Deyaan <65954790+Deyaan@users.noreply.github.com>
2021-05-08 12:06:43 +0530

    Update doctor.js

--- JS/doctor.js
+++ JS/doctor.js
@@ -1,108 +1,61 @@
 var id = 0;
-function addItemsToList(name,email,profession){
-    var ul = document.getElementById('list');
-    var header = document.createElement('h2');
+var docName, email;
+var docProfession;
+function addItemsToList(docName, email, docProfession) {
+  var ul = document.getElementById("list");
+  var header = document.createElement("h2");
 
-    var _name = document.createElement('li');
-    var _email=document.createElement('li');
-    var _profession=document.createElement('li');
+  var _name = document.createElement("li");
+  var _email = document.createElement("li");
+  var  _profession = document.createElement("li");
+  header.innerHTML = "" + docName;
 
-    header.innerHTML = '' + name;
+  _email.innerHTML = "Email: " + email;
+  _profession.innerHTML = "Profession: " + docProfession;
+  //console.log(docProfession)
 
-    _email.innerHTML='Email: '+email; 
-    _profession.innerHTML='Profession '+profession;
-    ul.appendChild(header);
-    ul.appendChild(_name);
-    ul.appendChild(_email);
-    ul.appendChild(_profession);
+  ul.appendChild(header);
+  ul.appendChild(_name);
+  ul.appendChild(_email);
+  ul.appendChild(_profession);
 }
 
-/*function FetchAllData(){
-    firebase.database().ref('Doctors').once('value',function(snapshot){
-        snapshot.forEach(
-            function(ChildSnapshot){
-                let name = ChildSnapshot.val().Name;
-                let email = ChildSnapshot.val().Email;
-                let profession = ChildSnapshot.val().Profession;
-                addItemsToList(name,email,profession);
-            }
-        )
+function Physio() {
+  firebase
+    .database()
+    .ref("Doctors/" + "Physiotherapists/")
+    .once("value", function (snapshot) {
+      snapshot.forEach(function (ChildSnapshot) {
+        docName = ChildSnapshot.val().Name;
+        email = ChildSnapshot.val().Email;
+        docProfession = ChildSnapshot.val().Profession;
+        addItemsToList(docName, email, docProfession);
+      });
     });
-}
-FetchAllData();
-*/
-function Physio(){
-    firebase.database().ref('Doctors').once('value',function(snapshot){
-        snapshot.forEach(
-            function(ChildSnapshot){
-                let name = ChildSnapshot.val().Name;
-                let email = ChildSnapshot.val().Email;
-                let profession = ChildSnapshot.val().Profession;
-                addItemsToList(name,email,profession);
-            }
-        )
-    });
+    return docProfession;
 }
 Physio();
 
-/*
-function Physio(){
-    firebase.database().ref('Doctors').once('value',function(snapshot){
-        snapshot.forEach(
-            function(ChildSnapshot){
-                let name = ChildSnapshot.val().Name;
-                let email = ChildSnapshot.val().Email;
-                let profession = ChildSnapshot.val().Profession;
-                addItemsToList(name,email,profession);
-            }
-        )
+function Cardio() {
+  firebase
+    .database()
+    .ref("Doctors/" + "Cardiologists/")
+    .once("value", function (snapshot) {
+      snapshot.forEach(function (ChildSnapshot) {
+        docName = ChildSnapshot.val().Name;
+        email = ChildSnapshot.val().Email;
+        docProfession = ChildSnapshot.val().Profession;
+        addItemsToList(docName, email, docProfession);
+      });
     });
 }
-function Derma(){
-    firebase.database().ref('Doctors').once('value',function(snapshot){
-        snapshot.forEach(
-            function(ChildSnapshot){
-                let name = ChildSnapshot.val().Name;
-                let email = ChildSnapshot.val().Email;
-                let profession = ChildSnapshot.val().Profession;
-                addItemsToList(name,email,profession);
-            }
-        )
-    });
-}
-
-function DoctorType(){
-    firebase.database().ref('Doctors').once('value',function(snapshot){
-        snapshot.forEach(
-            function(ChildSnapshot){
-                profession = ChildSnapshot.val().Profession;
-                addItemsToList(profession);
-                alert(profession);
-            }
-        )
-    });
-    if(profession==="Physiotherapists"){
+Cardio();
+function DoctorType(docProfession){
+    if(docProfession==="Physiotherapists"){
         Physio();
-        console.log("done!!")
     }
-    else if(profession=== "Dermatologists"){
-        Derma(); 
+    else if(docProfession=== "Cardiologists"){
+        Cardio(); 
       } 
 }
-DoctorType();
-*/
-
-/*function DoctorType(){
-    if(_profession===)
-}
-
-function doctortype(){ 
-    // fetch doctor profession form firebase databse 
-    //if(_profession=== "physio"){ fetchphsiodetails();
- }else if(_profession=== derma{
-      fetchdermadetails(); 
-    } 
-} 
-function fetch physiodetails()
-{ } 
-function fetch dermaDetails(){ } doctortype()*/
+//DoctorType(docProfession);
